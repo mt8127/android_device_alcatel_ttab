eae8a369582d99f01f422c5ffba982ec670acac7
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    ttab: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- lineage_ttab.mk
+++ lineage_ttab.mk
@@ -8,6 +8,7 @@ $(call inherit-product, device/alcatel/ttab/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
 
