f4749e052769476b83915d72ebfb258b71341fb5
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0200

    Consider lineage_ttab product via the AOSP  way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_ttab.mk
+    $(LOCAL_DIR)/lineage_ttab.mk
--- lineage_ttab.mk
+++ lineage_ttab.mk
@@ -1,12 +1,15 @@
-# Release name
-PRODUCT_RELEASE_NAME := ttab
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/alcatel/ttab/full_ttab.mk)
+$(call inherit-product, device/alcatel/ttab/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
+PRODUCT_CHARACTERISTICS := tablet
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ttab
@@ -14,5 +17,5 @@ PRODUCT_NAME := lineage_ttab
 PRODUCT_BRAND := alcatel
 PRODUCT_MODEL := ttab
 PRODUCT_MANUFACTURER := alcatel
-PRODUCT_RELEASE_NAME := ttab
+
 PRODUCT_GMS_CLIENTID_BASE := android-alcatel
